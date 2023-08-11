d6b9c69fc2eb9fd726a69fe382b8ef33330b24d4
lisnar <mail@lisnar.io>
2023-08-12 01:08:22 +0700

    setup: absolute imports

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
 import react from '@vitejs/plugin-react-swc';
+import { resolve } from 'node:path';
 import { defineConfig } from 'vite';
+
+const projectDir = resolve(__dirname);
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  // Enable bundler to resolve absolute path.
+  // https://vitejs.dev/config/shared-options.html#resolve-alias
+  // https://github.com/rollup/plugins/tree/master/packages/alias#entries
+  resolve: {
+    alias: [{ find: /^~\/(.*)/, replacement: `${projectDir}/src/$1` }],
+  },
 });
