c849bdb7cb839e8dba9af004b01706db64935853
lisnar <mail@lisnar.io>
2023-09-11 20:32:53 +0700

    story: controlled listbox

--- src/components/elements/ListBox/ListBox.stories.tsx
+++ src/components/elements/ListBox/ListBox.stories.tsx
@@ -1,5 +1,5 @@
 import { Meta, StoryObj } from '@storybook/react';
-import { Item, Section } from 'react-stately';
+import { Item, Section, useListData } from 'react-stately';
 import { ListBox } from './ListBox.tsx';
 
 const meta: Meta<typeof ListBox> = {
@@ -54,4 +54,41 @@ export const Multiple: Story = {
       </Section>
     </ListBox>
   ),
+};
+
+interface Item {
+  id: number;
+  name: string;
+  items?: Item[];
+}
+
+function ControlledListBox() {
+  const list = useListData<Item>({
+    initialItems: [
+      { id: 1, name: 'Aardvark' },
+      { id: 2, name: 'Kangaroo' },
+      { id: 3, name: 'Snake' },
+      { id: 4, name: 'Disabled Animal' },
+    ],
+    initialSelectedKeys: [3],
+    getKey: (item) => item.id,
+  });
+
+  return (
+    <ListBox
+      label="Animals" // if label is not provided, `aria-label` must be used
+      items={list.items}
+      selectionMode="single"
+      selectedKeys={list.selectedKeys}
+      disabledKeys={[4]}
+      // Using arrow function to suppress `@typescript-eslint/unbound-method` error.
+      onSelectionChange={(keys) => list.setSelectedKeys(keys)}
+    >
+      {(item) => <Item>{item.name}</Item>}
+    </ListBox>
+  );
+}
+
+export const Controlled: Story = {
+  render: ControlledListBox,
 };
