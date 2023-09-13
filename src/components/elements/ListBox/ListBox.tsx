b6776335beeb96c42fb22883817aa0c8d5ada627
lisnar <mail@lisnar.io>
2023-09-13 18:28:55 +0700

    refactor: simplify listbox component

--- src/components/elements/ListBox/ListBox.tsx
+++ src/components/elements/ListBox/ListBox.tsx
@@ -1,12 +1,5 @@
 import { useRef } from 'react';
-import {
-  AriaListBoxProps,
-  mergeProps,
-  useHover,
-  useListBox,
-  useListBoxSection,
-  useOption,
-} from 'react-aria';
+import { AriaListBoxProps, useListBox, useListBoxSection, useOption } from 'react-aria';
 import { ListState, Node, useListState } from 'react-stately';
 import { CheckIcon } from '../../icons/CheckIcon.tsx';
 import { classNames } from '../utils.ts';
@@ -15,7 +8,11 @@ export function ListBox<T extends object>(props: AriaListBoxProps<T>) {
   const state = useListState(props);
   const ref = useRef(null);
 
-  const { listBoxProps, labelProps } = useListBox(props, state, ref);
+  const { listBoxProps, labelProps } = useListBox(
+    { ...props, shouldFocusOnHover: true },
+    state,
+    ref,
+  );
 
   return (
     <div className="w-60">
@@ -49,56 +46,33 @@ function ListBoxOption<T extends object>(props: ListBoxItemProps<T>) {
   const { item, state } = props;
   const ref = useRef(null);
 
-  const { optionProps, descriptionProps, isSelected, isFocused, isFocusVisible, isDisabled } =
-    useOption(item, state, ref);
-  const { hoverProps, isHovered } = useHover({
-    isDisabled: state.selectionManager.isDisabled(item.key),
-    onHoverStart() {
-      // Use this because `shouldFocusOnHover` in `useOption` is deprecated.
-      if (!isFocusVisible) {
-        // state.selectionManager.setFocused(true);
-        state.selectionManager.setFocusedKey(item.key);
-      }
-    },
-  });
+  const { optionProps, descriptionProps, isSelected, isDisabled } = useOption(item, state, ref);
 
   return (
     <li
-      {...mergeProps(optionProps, hoverProps)}
+      {...optionProps}
       ref={ref}
       className={classNames(
-        'relative cursor-default select-none', // behavior
+        'group relative cursor-default select-none', // behavior
         'py-2 pl-3 pr-9', // appearance
         'outline-none focus:bg-indigo-600 focus:text-white', // focused state
-        'data-[disabled]:bg-gray-100', // disabled state
+        'data-[disabled]:pointer-events-none data-[disabled]:bg-gray-100', // disabled state
       )}
       data-selected={isSelected || undefined}
-      data-hovered={isHovered || undefined}
       data-disabled={isDisabled || undefined}
     >
-      <div className="flex">
-        <span
-          className="truncate font-normal text-gray-900 data-[selected]:font-bold data-[disabled]:text-gray-400 data-[focused]:text-white"
-          data-selected={isSelected || undefined}
-          data-focused={isFocused || undefined}
-          data-disabled={isDisabled || undefined}
-        >
-          {item.rendered}
-        </span>
-        <span
-          {...descriptionProps}
-          className="ml-2 truncate text-gray-500 data-[disabled]:text-gray-400 data-[focused]:text-indigo-200"
-          data-focused={isFocused || undefined}
-          data-disabled={isDisabled || undefined}
-        >
-          @{item.type}
-        </span>
-      </div>
+      <span className="truncate font-normal text-gray-900 group-focus:text-white group-data-[selected]:font-bold group-data-[disabled]:text-gray-400">
+        {item.rendered}
+      </span>
+      {/* TODO: use context to get descriptionProps and pass component from `item.rendered` */}
+      <span
+        {...descriptionProps}
+        className="ml-2 truncate text-gray-500 group-focus:text-indigo-200 group-data-[disabled]:text-gray-400"
+      >
+        @{item.type}
+      </span>
       {isSelected && (
-        <div
-          className="absolute inset-y-0 right-0 flex items-center pr-4 text-indigo-600 data-[focused]:text-white"
-          data-focused={isFocused || undefined}
-        >
+        <div className="absolute inset-y-0 right-0 flex items-center pr-4 text-indigo-600 group-focus:text-white">
           <CheckIcon className="h-5 w-5" aria-hidden="true" />
         </div>
       )}
@@ -113,7 +87,7 @@ function ListBoxSection<T extends object>({ item, state }: ListBoxItemProps<T>) 
   });
 
   // Get the child items for the section. (`item.childNodes` is deprecated)
-  const childNodes = state.collection.getChildren?.(item.key) ?? [];
+  const childNodes = state.collection.getChildren!(item.key);
 
   // If the section is not the first, add a separator element to provide visual separation.
   // The heading is rendered inside an <li> element, which contains
